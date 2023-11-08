46deeea2cace4f6c1010a78a2c0007361b04facb
mcscorazza <mcs.corazza@gmail.com>
2023-11-08 16:21:00 -0300

    asas

--- index.php
+++ index.php
@@ -6,35 +6,14 @@ $server = '53e69abd7b384d66a432943bf26f295f.s2.eu.hivemq.cloud';     // change i
 $port = 8883;                     // change if necessary
 $username = 'gb_digital';                   // set your username
 $password = 'Gbds9159';                   // set your password
-$cliente_id = '';
+$client_id = '';
 
-echo "Iniciando MQTT . . . <br></br>";
-
-$mqtt = new Bluerhinos\phpMQTT($server, $port, $cliente_id);
-
-echo "Iniciando . . . </br>";
-
-echo "Verificando se está conectado . . . ";
-
+$mqtt = new Bluerhinos\phpMQTT($server, $port, $client_id);
 if(!$mqtt->connect(true, NULL, $username, $password)) {
-	echo "Erro de conexão!";
-    exit(1);
+    echo "ERRO!";
+	exit(1);
 }
 
-echo "OK!<br><br>";
-
-$mqtt->debug = true;
-$topics['teste/teste'] = array('qos' => 1, 'function' => 'procMsg');
-$mqtt->subscribe($topics, 1);
-
-while($mqtt->proc()) {
-
-}
+echo $mqtt->subscribeAndWaitForMessage('teste/teste', 1);
 
 $mqtt->close();
-
-function procMsg($topic, $msg){
-		echo 'Msg Recieved: ' . date('r') . "\n";
-		echo "Topic: {$topic}\n\n";
-		echo "\t$msg\n\n";
-}
