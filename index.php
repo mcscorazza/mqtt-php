e954f2d2232b3a99cf66af217549f5055f0e9e34
mcscorazza <mcs.corazza@gmail.com>
2023-11-08 15:12:19 -0300

    b

--- index.php
+++ index.php
@@ -7,13 +7,27 @@ $port = 8883;                     // change if necessary
 $username = 'gbdigital';                   // set your username
 $password = '##Gbds9159##';                   // set your password
 $client_id = 'phpMQTT-publisher'; // make sure this is unique for connecting to sever - you could use uniqid()
-$cafile = 'isrgrootx1.pem'; // HiveMQ Cloud CA
+//$cafile = 'isrgrootx1.pem'; // HiveMQ Cloud CA
 
 $mqtt = new Bluerhinos\phpMQTT($server, $port, $client_id, $cafile);
 
-if ($mqtt->connect(true, NULL, $username, $password)) {
-	$mqtt->publish('teste/teste', 'Hello World! at ' . date('r'), 0, false);
-	$mqtt->close();
-} else {
-    echo "Time out!\n";
+if(!$mqtt->connect(true, NULL, $username, $password)) {
+	exit(1);
+}
+
+$mqtt->debug = true;
+
+$topics['teste/teste'] = array('qos' => 0, 'function' => 'procMsg');
+$mqtt->subscribe($topics, 0);
+
+while($mqtt->proc()) {
+
+}
+
+$mqtt->close();
+
+function procMsg($topic, $msg){
+    echo 'Msg Recieved: ' . date('r') . "\n";
+    echo "Topic: {$topic}\n\n";
+    echo "\t$msg\n\n";
 }
