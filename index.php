56e099ac18920233a3f4de1d2f9b8b43a42f96c2
mcscorazza <mcs.corazza@gmail.com>
2023-11-08 15:06:55 -0300

    a

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+require('./phpMQTT.php');
+
+$server = '53e69abd7b384d66a432943bf26f295f.s2.eu.hivemq.cloud';     // change if necessary
+$port = 8883;                     // change if necessary
+$username = 'gbdigital';                   // set your username
+$password = '##Gbds9159##';                   // set your password
+$client_id = 'phpMQTT-publisher'; // make sure this is unique for connecting to sever - you could use uniqid()
+$cafile = 'isrgrootx1.pem'; // HiveMQ Cloud CA
+
+$mqtt = new Bluerhinos\phpMQTT($server, $port, $client_id, $cafile);
+
+if ($mqtt->connect(true, NULL, $username, $password)) {
+	$mqtt->publish('teste/teste', 'Hello World! at ' . date('r'), 0, false);
+	$mqtt->close();
+} else {
+    echo "Time out!\n";
+}
